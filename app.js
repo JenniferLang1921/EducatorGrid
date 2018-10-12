dcd2432c66ceac2f8cf9b17e25bbb8798b4d3911
j690e533 <jenniferlang1921@gmail.com>
2018-10-12 13:20:16 -0500

    click message

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+$( document ).ready(function() {
+    $("i").click(function(){
+        $("#message").toggleClass("hidden");
+
+               console.log("working");
+    });
+    });
+
+
+
+
 $('#general').tab('show') 
 $('#InstructionallyEmbedded').tab('show') 
 $('#yearend').tab('show') 
 $('#studentarchive').tab('show') 
 $('#extracts').tab('show'); 
 
-$( document ).ready(function() {
-$( "#question" ).click(function() {
-    $( ".message" ).toggle();
-    console.log("working");
-    });
 
-});
-  
